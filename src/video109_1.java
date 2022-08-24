292dcd2927645850b8e400d0be883dca01d656fd
enosisBD <maruf.monem@enosisbd.com>
2022-08-24 09:18:15 +0600

    Using soft assertion

--- src/video109_1.java
+++ src/video109_1.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 public class video109_1 {
 
@@ -19,6 +20,7 @@ public class video109_1 {
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
 		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+		SoftAssert sa = new SoftAssert();
 		
 		for (WebElement link : links) {
 //			Getting the url link
@@ -33,14 +35,12 @@ public class video109_1 {
 //			Getting the response code
 			int res = conn.getResponseCode();
 			System.out.println("Response: " + res + " --- " + urlText + " ---> " + url );
-			
-//			Assert when fail
-			if(res>400) {
-				System.out.println("Failed! Response: " + res + " --- " + urlText + " ---> " + url );
-				Assert.assertTrue(false);
-			}
+//			Assert when fail - soft assertion
+			sa.assertTrue(res<400, "Failed! Response: " + res + " --- " + urlText + " ---> " + url);
+
 			
 		}
+		sa.assertAll();
 	
 	}
 
