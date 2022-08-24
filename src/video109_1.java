8d5ad2533ae721da3a265b49bd26f0c29879e114
enosisBD <maruf.monem@enosisbd.com>
2022-08-24 09:03:21 +0600

    Navigating through all links with hot assertion

--- src/video109_1.java
+++ src/video109_1.java
@@ -3,10 +3,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class video109_1 {
 
@@ -15,20 +18,30 @@ public class video109_1 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
-//		Broken URL
-//		Step 1: Get all the URLs
-//		Step 2: Use java methods to get the status code
-//		Check if the status code is >400
-		String url = driver.findElement(By.cssSelector("a[href*='broken']")).getAttribute("href");
-		HttpURLConnection conn= (HttpURLConnection)new URL(url). openConnection();
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
 		
-		conn.setRequestMethod("HEAD");
-		conn.connect();
-		int res = conn.getResponseCode();
-		System.out.println(res);
-		
+		for (WebElement link : links) {
+//			Getting the url link
+			String url = link.getAttribute("href");
+//			The link name
+			String urlText = link.getText();
+//			Creating the HTTP connection 
+			HttpURLConnection conn= (HttpURLConnection)new URL(url). openConnection();
+			conn.setRequestMethod("HEAD");
+//			Connecting using head method
+			conn.connect();
+//			Getting the response code
+			int res = conn.getResponseCode();
+			System.out.println("Response: " + res + " --- " + urlText + " ---> " + url );
+			
+//			Assert when fail
+			if(res>400) {
+				System.out.println("Failed! Response: " + res + " --- " + urlText + " ---> " + url );
+				Assert.assertTrue(false);
+			}
+			
+		}
 	
-		
 	}
 
 }
