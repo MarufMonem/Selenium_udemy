33dbfc137c3fd3992a15d68e33be59156a6f4376
enosisBD <maruf.monem@enosisbd.com>
2022-08-29 09:59:56 +0600

    Revert "Different ways of writing streams"
    This reverts commit b7ab5d207904572abd0cd2cefb99aa2d1434e622.

--- src/Java_Streams/video115_1.java
+++ src/Java_Streams/video115_1.java
@@ -2,16 +2,15 @@ package Java_Streams;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import javax.xml.stream.StreamFilter;
 
 import org.testng.annotations.Test;
 
 public class video115_1 {
-	@Test
+@Test
 	public  static void regular() {
-		//		Count the names that start with A
+//		Count the names that start with A
 		List<String> names  = new ArrayList<String>();
 		names.add("Aron");
 		names.add("Tron");
@@ -19,7 +18,7 @@ public class video115_1 {
 		names.add("Ghalib");
 		names.add("Aibby");
 		int count =0;
-
+		
 		for (String name : names) {
 			char characters[] = name.toCharArray();
 			if(characters[0] == 'A') {
@@ -37,29 +36,16 @@ public class video115_1 {
 		names.add("Daisy");
 		names.add("Ghalib");
 		names.add("Aibby");
-
-		//		Making the array list into streams. Streams are a collection of strings
-		//		Filter method would filter based on the condtions provided
+		
+//		Making the array list into streams. Streams are a collection of strings
+//		Filter method would filter based on the condtions provided
 		Long c = names.stream().filter(s-> s.startsWith("A")).count();
 		System.out.println(c);
 	}
 
 	public static void main(String argsp[]) {
-//		streamFilter();
-
-		long d = Stream.of("Arum", "Sarath", "Rahim", "Asish").filter(s-> 
-		{
-			return s.startsWith("A");
-		}
-				).filter(p->
-				{
-					return p.endsWith("m");
-				}
-						).count(); 
-		
-		System.out.println(d);
+		streamFilter();
 	}
 	
-
 
 }
