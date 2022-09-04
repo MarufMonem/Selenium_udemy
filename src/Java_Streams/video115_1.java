19f07b454372fb315baf8778bdb28f872471af38
enosisBD <maruf.monem@enosisbd.com>
2022-09-04 11:39:24 +0600

    merging streams and more examples of maps

--- src/Java_Streams/video115_1.java
+++ src/Java_Streams/video115_1.java
@@ -1,11 +1,13 @@
 package Java_Streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import javax.xml.stream.StreamFilter;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class video115_1 {
@@ -65,18 +67,32 @@ public class video115_1 {
 	@Test
 	public static void streamMap(){
 //		print names with last letter h
-		Stream.of("Arum", "Sarath", "Rahim", "Asish")
+		Stream.of("Azrum", "Sarath", "Rahim", "Asish")
 				.filter(s -> s.endsWith( "h"))
 				.map(s -> s.toUpperCase())
 				.forEach(s -> System.out.println(s));
+		System.out.println("----------------------");
+//		Print names which have first letter as A with uppercase and sorted
+		String a[] = {"Azrum", "Sarath", "Rahim", "Asish"};
+		List<String> names = Arrays.asList(a);
+		names.stream()
+				.filter(s -> s.startsWith("A"))
+				.sorted().map(s -> s.toUpperCase())
+				.forEach(s -> System.out.println(s));
 	}
-	
+//Mergingg to lists
+	@Test
+	public static void mergingStreams(){
+		String a1[] = {"Azrum", "Sarath", "Rahim", "Asish"};
+		String a2[] = {"Anish", "Harish", "Chenu", "Diviya", "Micheal"};
+		Stream<String> newStream =  Stream.concat(Arrays.stream(a1), Arrays.stream(a2));
+//		newStream.sorted().forEach(s -> System.out.println(s));
 
-	public static void main(String args[]) {
-//		streamFilter();
-//		multipleFilters();
-
+		Boolean flag = newStream.anyMatch(s -> s.equalsIgnoreCase("Sarath"));
+		System.out.println(flag);
+		Assert.assertTrue(flag);
 	}
+
 	
 
 }
