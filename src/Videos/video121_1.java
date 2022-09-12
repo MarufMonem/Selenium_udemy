feec6ddfd03dc843e22502acc93b04237ceabe14
enosisBD <maruf.monem@enosisbd.com>
2022-09-12 09:17:07 +0600

    Relative locator: above()

--- src/Videos/video121_1.java
+++ src/Videos/video121_1.java
@@ -3,15 +3,17 @@ package Videos;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
 
 public class video121_1 {
     public static void main(String[] args){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+        String output = driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText();
+        System.out.println(output);
     }
 }
